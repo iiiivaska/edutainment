c2b3378c156318f677e9765039826f8dd295595f
iiiivaska <iiiivaska@yandex.ru>
2021-10-27 16:19:33 +0300

    Initial Commit

---
+++ edutainment/edutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainmentApp.swift
+//  edutainment
+//
+//  Created by Василий Буланов on 10/27/21.
+//
+
+import SwiftUI
+
+@main
+struct edutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
